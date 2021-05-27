45a68bfda4cb7932d8cece15b0c3dc34df95be7d
gr-kim-94 <muuulll@jj.ac.kr>
2021-05-27 23:55:40 +0900

    Initial Commit

---
+++ MooMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MooMemo
+//
+//  Created by 김가람 on 2021/05/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
